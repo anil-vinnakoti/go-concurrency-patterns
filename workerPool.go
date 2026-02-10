ecce9abff976d2c3f27441eff707ce2404cb304a
Anil Vinnakoti <141225368+anil-vinnakoti@users.noreply.github.com>
2026-02-10 17:39:25 +0000

    udpdate code to usual worker pool pattern

--- workerPool.go
+++ workerPool.go
@@ -1,59 +1,80 @@
 package main
 
 import (
-	"context"
 	"fmt"
+	"net/http"
 	"sync"
 	"time"
 )
 
-func workerPool() {
-	userIDs := make([]int, 100)
-	for i := range userIDs {
-		userIDs[i] += i + 1
-	}
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	var wg sync.WaitGroup
-
-	numsOfWorkers := 10
-	jobChannel := make(chan int)
-
-	// start workers
-	for i := 0; i < numsOfWorkers; i++ {
-		wg.Add(1)
-		go func(workerID int) {
-			defer wg.Done()
-			for {
-				select {
-				case <-ctx.Done():
-					fmt.Println("printing before context cancelling...")
-					return
-				case jobID, ok := <-jobChannel:
-					if !ok {
-						return
-					}
-					processUser(ctx, jobID, workerID)
-				}
-			}
-
-		}(i)
-
-	}
-
-	// send jobs
-	for _, userID := range userIDs {
-		jobChannel <- userID
-	}
-
-	close(jobChannel) // important
-	wg.Wait()
-	fmt.Println("printing after waiting from waitGroup...")
+type WebSite struct {
+	URL string
 }
 
-func processUser(ctx context.Context, jobID int, workerID int) {
-	fmt.Printf("worker %d processing user %d\n", workerID, jobID)
-	time.Sleep(time.Millisecond * 200)
+type Result struct {
+	workerID int
+	URL      string
+	Status   int
+}
+
+func crawler(workedID int, jobs <-chan WebSite, result chan<- Result, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	client := http.Client{
+		Timeout: 5 * time.Second,
+	}
+
+	for job := range jobs {
+		resp, err := client.Get(job.URL)
+		if err != nil {
+			result <- Result{URL: job.URL, Status: 400, workerID: workedID}
+			continue
+		}
+		result <- Result{URL: job.URL, Status: resp.StatusCode, workerID: workedID}
+		resp.Body.Close()
+	}
+}
+
+func workerPool() {
+	var wg sync.WaitGroup
+
+	jobsChannel := make(chan WebSite)
+	resultChannel := make(chan Result)
+
+	urls := []string{
+		"https://www.google.com",
+		"https://duckduckgo.com/",
+		"https://www.bing.com/",
+		"https://www.startpage.com/",
+		"https://www.ecosia.org/",
+		"https://www.qwant.com/",
+		"https://www.kagi.com/",
+	}
+
+	// start workers
+	for i := 1; i < 4; i++ {
+		wg.Add(1)
+		go crawler(i, jobsChannel, resultChannel, &wg)
+	}
+
+	// close result channel after workers finish
+	go func() {
+		wg.Wait()
+		close(resultChannel)
+	}()
+
+	// send jobs
+	go func() {
+		for _, url := range urls {
+			jobsChannel <- WebSite{URL: url}
+		}
+		close(jobsChannel)
+	}()
+
+	// collect results
+	for result := range resultChannel {
+		fmt.Printf("Worker %d processed %s with status %d\n",
+			result.workerID, result.URL, result.Status)
+	}
+
 }
